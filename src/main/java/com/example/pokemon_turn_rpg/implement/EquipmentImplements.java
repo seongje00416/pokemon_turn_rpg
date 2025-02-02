357b3cb62310c35ee5fefb33d35ad5d6f3b53698
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-02-02 18:53:57 +0900

    feat: Implement 파일 상속 메소드 틀 제작

--- src/main/java/com/example/pokemon_turn_rpg/implement/EquipmentImplements.java
+++ src/main/java/com/example/pokemon_turn_rpg/implement/EquipmentImplements.java
@@ -1,8 +1,15 @@
 package com.example.pokemon_turn_rpg.implement;
 
+import com.example.pokemon_turn_rpg.common.SuccessResponse;
 import com.example.pokemon_turn_rpg.controller.EquipmentController;
+import com.example.pokemon_turn_rpg.dto.request.ChangeNormalEquipmentRequest;
+import com.example.pokemon_turn_rpg.dto.response.GetHoldItemResponse;
+import com.example.pokemon_turn_rpg.dto.response.GetUserNormalEquipmentResponse;
+import com.example.pokemon_turn_rpg.dto.response.GetUserSpecialEquipmentResponse;
+import com.example.pokemon_turn_rpg.dto.response.GetWearEquipmentResponse;
 import com.example.pokemon_turn_rpg.service.EquipmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EquipmentImplements implements EquipmentController {
     private EquipmentService equipmentService;
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetWearEquipmentResponse>> getWearEquipment(Long userPokemonId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> changeNormalEquipment(ChangeNormalEquipmentRequest changeNormalEquipmentRequest) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> changeSpecialEquipment(Long userPokemonId, Long specialEquipmentId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> changeHoldItem(Long userPokemonId, Long holdItemId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetUserNormalEquipmentResponse>> getUserNormalEquipmentList(Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetUserSpecialEquipmentResponse>> getUserSpecialEquipmentList(Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetHoldItemResponse>> getUserHoldItemList(Long userId) {
+        return null;
+    }
 }
--- src/main/java/com/example/pokemon_turn_rpg/implement/UserImplements.java
+++ src/main/java/com/example/pokemon_turn_rpg/implement/UserImplements.java
@@ -1,8 +1,14 @@
 package com.example.pokemon_turn_rpg.implement;
 
+import com.example.pokemon_turn_rpg.common.SuccessResponse;
 import com.example.pokemon_turn_rpg.controller.UserController;
+import com.example.pokemon_turn_rpg.dto.request.ChangeProfileImageRequest;
+import com.example.pokemon_turn_rpg.dto.request.UseUserMoneyRequest;
+import com.example.pokemon_turn_rpg.dto.request.UserExpRequest;
+import com.example.pokemon_turn_rpg.dto.response.*;
 import com.example.pokemon_turn_rpg.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserImplements implements UserController {
     private UserService userService;
 
+    @Override
+    public ResponseEntity<SuccessResponse<GetUserProfileResponse>> getUserProfile(Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> changeProfileImage(ChangeProfileImageRequest changeProfileImageRequest) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> changeProfileNickname(Long userId, String newNickname) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<UserExpResponse>> userExpUpdate(UserExpRequest userExpRequest) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> addUserPokemon(Long userId, Long pokemonId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetUserPokemonListResponse>> getUserPokemonList(Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetUserPokemonDetailResponse>> getUserPokemonDetail(Long userPokemonId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<GetUserMoneyResponse>> getUserMoney(Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SuccessResponse<Void>> useUserMoney(UseUserMoneyRequest useUserMoneyRequest) {
+        return null;
+    }
 }
